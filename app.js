52db5e5e227bcffcf82e9bb0cfcd3e7ed73e102c
celinedesnoe <celine.desnoe@skema.edu>
2019-02-11 11:22:03 +0100

    Search page ok

--- app.js
+++ app.js
@@ -55,4 +55,7 @@ app.locals.title = "Express - Generated with IronGenerator";
 const index = require("./routes/index");
 app.use("/", index);
 
+const search = require("./routes/search-router.js");
+app.use("/", search);
+
 module.exports = app;
--- routes/search-router.js
+++ routes/search-router.js
@@ -1,9 +1,9 @@
 const express = require("express");
+const Cocktail = require("../models/cocktail-model.js");
 const router = express.Router();
-const Cocktail = require("..models/cocktail-model.js");
 
 router.get("/search", (req, res, next) => {
-  res.render("search.hbs");
+  res.render("search-views/search.hbs");
 });
 
 // router.get("/search-results", (req,res,next)) => {
