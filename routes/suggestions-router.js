8a30aa288a5cfdc2d6fbf5f8d4c8d62a4cfe7f2b
Christophe Begue <Christophe@MacBook-Pro-de-MACBOOK-5.local>
2019-02-11 16:22:36 +0100

    ingredient list added

--- routes/suggestions-router.js
+++ routes/suggestions-router.js
@@ -3,11 +3,75 @@ const router = express.Router();
 const Cocktail = require("../models/cocktail-model.js");
 
 router.get("/suggestions", (req, res, next) => {
-  Cocktail.find()
-    .then(cocktailList => {
-      console.log(cocktailList, "fofofofofoff");
-      res.locals.allcocktails = cocktailList;
-      res.render("suggestion-views/bar-input.hbs");
+  var ingredients = [];
+  // function uniq(ingredients) {
+  //   return ingredients.sort().filter(function(item, pos, ary) {
+  //     return !pos || item != ary[pos - 1];
+  //   });
+  // }
+  Cocktail.distinct(`strIngredient9`).then(ingredResults => {
+    ingredResults.forEach(ingredient => {
+      ingredients.push(ingredient);
+      console.log("ingredient9");
+    });
+  });
+  Cocktail.distinct(`strIngredient8`).then(ingredResults => {
+    ingredResults.forEach(ingredient => {
+      ingredients.push(ingredient);
+      console.log("ingredient8");
+    });
+  });
+  Cocktail.distinct(`strIngredient7`).then(ingredResults => {
+    ingredResults.forEach(ingredient => {
+      ingredients.push(ingredient);
+      console.log("ingredient7");
+    });
+  });
+  Cocktail.distinct(`strIngredient6`).then(ingredResults => {
+    ingredResults.forEach(ingredient => {
+      ingredients.push(ingredient);
+      console.log("ingredient6");
+    });
+  });
+  Cocktail.distinct(`strIngredient5`).then(ingredResults => {
+    ingredResults.forEach(ingredient => {
+      ingredients.push(ingredient);
+      console.log("ingredient5");
+    });
+  });
+  Cocktail.distinct(`strIngredient4`).then(ingredResults => {
+    ingredResults.forEach(ingredient => {
+      ingredients.push(ingredient);
+      console.log("ingredient4");
+    });
+  });
+  Cocktail.distinct(`strIngredient3`).then(ingredResults => {
+    ingredResults.forEach(ingredient => {
+      ingredients.push(ingredient);
+      console.log("ingredient3");
+    });
+  });
+  Cocktail.distinct(`strIngredient2`).then(ingredResults => {
+    ingredResults.forEach(ingredient => {
+      ingredients.push(ingredient);
+      console.log("ingredient2");
+    });
+  });
+  Cocktail.distinct(`strIngredient1`)
+    .then(ingredResults => {
+      ingredResults.forEach(ingredient => {
+        ingredients.push(ingredient);
+        console.log("ingredient1");
+      });
+      var lowerCaseIngredients = ingredients.map(function(oneIngredient) {
+        return oneIngredient.toLowerCase();
+      });
+      var uniqueIngredients = lowerCaseIngredients.filter(function(item, pos) {
+        return lowerCaseIngredients.indexOf(item) == pos;
+      });
+
+      res.locals.ingredArray = uniqueIngredients;
+      res.render("suggestion-views/bar-input.hbs", ingredResults);
     })
     .catch(err => next(err));
 });
