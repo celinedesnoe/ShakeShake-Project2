7e2667f2c78aadc1ad33df71ac2a06ddc9738e7a
Christophe Begue <Christophe@MacBook-Pro-de-MACBOOK-5.local>
2019-02-15 15:38:26 +0100

    demo2

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ hbs.registerHelper(helpers);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 app.use(express.static(path.join(__dirname, "public")));
-app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
+app.use(favicon(path.join(__dirname, "public", "images", "favicon.png")));
 
 app.use(
   session({
